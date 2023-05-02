1c8f589beebf290a4047e1da70a3a106cea11b17
davidveloso <velosoda0159@outlook.com>
2023-05-02 00:17:05 -0400

    Express is setup in a basic form, Mongose setup is here but not tested

--- gladiator-web/client/src/App.js
+++ gladiator-web/client/src/App.js
@@ -1,17 +1,44 @@
 import './App.css';
-import React from 'react'
+import * as React from 'react'
+import { useState } from 'react';
 
-function App() {
-  const [posts, setPosts] = React.useState(null);
+
+export const TestComponent = () => {
+
+  const [posts, setPosts] = useState([{}]);
+
+  // useEffect(() => {
+  //   fetch("/api/posts/").then(
+  //     result => result.json()
+  //   ).then(
+  //     data => {
+  //       setPosts(data)
+  //     });
+  // }, [])
+
   const getPosts = () => {
-    fetch('/api/posts/')
-      .then(result => result.json())
-      .then(body => setPosts(body));
+    fetch("/api/posts/").then(
+      result => result.json()
+    ).then(
+      data => {
+        setPosts(data)
+      });
+    console.log(posts);
   };
+
   return (
     <div className="app">
       <button onClick={getPosts}>Get Posts</button>
+      {(typeof posts.posts === 'undefined') ? (
+        <p>loading....</p>
+      ):(
+        posts.posts.map((post, i) => (
+          <div key={i}>
+          <p>[id: {post.id}]</p>
+          <p>[title: {post.title}]</p>
+        </div>
+        ))
+      )}
     </div>
   );
 }
-export default App;
--- gladiator-web/server/index.js
+++ gladiator-web/server/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://velosoda0159:kNgourv3uaooIwkG@gladiatorwebapp.dk8nzgr.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const port = 4000;
 
 app.get('/api/posts', (req, res) => {
-  const posts = [
-    { id: 1, title: 'Post 1' },
-    { id: 2, title: 'Post 2' },
-    { id: 3, title: 'Post 3' }
-  ];
+  const posts = {
+    "posts" : [
+      { id: 1, title: 'Post 1' },
+      { id: 2, title: 'Post 2' },
+      { id: 3, title: 'Post 3' }
+    ]
+  }
+  console.log(`request recieved! ${posts}`);
   res.json(posts);
 });
 
